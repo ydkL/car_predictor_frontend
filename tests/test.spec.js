4f45cfa79b0e45aa441d677e75b7236e38c3a5c9
Yusuf Dudaklı <yusuf.dudakli@arksigner.com>
2024-05-17 14:02:09 +0300

    test commit

---
+++ tests/test.spec.js
@@ -0,0 +1,7 @@
+const {test, expect} = require('@playwright/test');
+
+test('first test', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    const title = await page.title();
+    expect(title).toBe('React App');
+});
