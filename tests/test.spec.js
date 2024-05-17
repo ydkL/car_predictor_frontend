3b4c737822b794717f15cfbcfe290d1a8d8887e6
Yusuf Dudaklı <152588946+ydkL@users.noreply.github.com>
2024-05-17 11:08:56 +0000

    test update

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -3,5 +3,5 @@ const {test, expect} = require('@playwright/test');
 test('first test', async ({ page }) => {
     await page.goto('http://localhost:3000');
     const title = await page.title();
-    expect(title).toBe('React App');
+    expect(title).toBe('Resact App');
 });
